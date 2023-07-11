b9f0a344568bdf76bfe984267bdd04fda938adca
salvomc <smcrwrk@gmail.com>
2023-07-11 11:24:07 +0200

    array products added

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php
 
+require_once __DIR__."/Models/products.php";
+require_once __DIR__."/Models/food.php";
+require_once __DIR__."/Models/accessories.php";
+require_once __DIR__."/Models/games.php";
+
+$products=
+[
+    new Food('https://www.pets-house.it/5767-large_default/royal-canin-mini-adult-800-gr.jpg', 'Royal Canin Mini Adult', 'Cane <i class="fa-solid fa-dog"></i>', 'Prezzo: € 43,99', 'Peso netto: 545g', 'Ingredienti: prosciutto, riso'),
+
+    new Food('https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 'Almo Nature Holistic Maintenance Medium Large Tonno e Riso', 'Cane <i class="fa-solid fa-dog"></i>', 'Prezzo: € 44,99', 'Peso netto: 600g', 'Ingredienti: manzo, cereali'),
+
+    new Food('https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 'Almo Nature Cat Daily Lattina', 'Gatto <i class="fa-solid fa-cat"></i>', 'Prezzo: € 34,99', 'Peso netto: 400g', 'Ingredienti: tonno, pollo, prosciutto'),
+
+    new Foods('https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 'Mangime per Pesci Guppy in Fiocchi', 'Pesce <i class="fa-solid fa-fish-fins"></i>', 'Prezzo: € 2,95', 'Peso netto: 30g', 'Ingredienti: Pesci e sottoprodotti dei pesci, Cereali, Lieviti, Alghe'),
+
+    new Accessories('https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', 'Voliera Wilma in Legno', 'Uccello <i class="fa-solid fa-dove"></i>', 'Prezzo: € 184,99', 'Materiale: Legno', 'Dimensioni: M: L 83 x P 67 x H 153 cm'),
+
+    new Accessories('https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Cartucce Filtranti per Filtro EasyCrystal', 'Pesce <i class="fa-solid fa-fish-fins"></i>', 'Prezzo: € 2,29', 'Materiale: Materiale espanso', 'Dimensioni: ND'),
+
+    new Games('https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg', 'Kong Classic', 'Cane <i class="fa-solid fa-dog"></i>', 'Prezzo: € 13,49', 'Caratteristiche: Galleggia e rimbalza', 'Dimensioni: 8,5 cm x 10 cm'),
+
+    new Games('https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', 'Topini di peluche Trixie', 'Gatto <i class="fa-solid fa-cat"></i>', 'Prezzo: € 4,99', 'Caratteristiche: Morbido con codina in corda', 'Dimensioni: 8,5 cm x 10 cm')
+]
+
 ?>
